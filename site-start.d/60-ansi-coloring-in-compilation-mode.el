82b993fe69703e90b236dd2431667fe97b403524
Adam Chyła <adam@chyla.org>
2019-04-12 00:36:52 +0200

    add ANSI coloring in compilation mode

---
+++ site-start.d/60-ansi-coloring-in-compilation-mode.el
@@ -0,0 +1,13 @@
+; ANSI Coloring in Compilation Mode
+; https://stackoverflow.com/questions/13397737/ansi-coloring-in-compilation-mode
+
+(message "Setting ANSI coloring in compilation mode...")
+
+(ignore-errors
+  (require 'ansi-color)
+  (defun my-colorize-compilation-buffer ()
+    (when (eq major-mode 'compilation-mode)
+      (ansi-color-apply-on-region compilation-filter-start (point-max))))
+  (add-hook 'compilation-filter-hook 'my-colorize-compilation-buffer))
+
+(message "Setting ANSI coloring in compilation mode... Done.")
