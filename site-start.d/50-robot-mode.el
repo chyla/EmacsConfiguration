7e7256f207f1abfaecae190a6301a918877e43d9
Adam Chyła <adam@chyla.org>
2019-04-23 23:30:08 +0200

    add robot-mode

---
+++ site-start.d/50-robot-mode.el
@@ -0,0 +1,9 @@
+(message "Loading robot-mode settings...")
+
+(load-file "~/.emacs.d/extensions/robot-mode/robot-mode.el")
+(add-to-list 'auto-mode-alist '("\\.robot\\'" . robot-mode))
+
+(define-key robot-mode-map [remap xref-find-definitions] 'robot-mode-find-kw)
+
+(message "Loading robot-mode settings... Done.")
+
