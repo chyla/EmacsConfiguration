a20401d6c55560d38c4f78d984d652b021f18f86
Adam Chyła <adam@chyla.org>
2019-04-25 00:52:11 +0200

    add import snippet for ttcn-3

--- site-start.d/50-yasnippet.el
+++ site-start.d/50-yasnippet.el
@@ -1,5 +1,10 @@
 (message "Loading yasnippet settings...")
 
+(setq yas-snippet-dirs
+      '("~/.emacs.d/snippets"                 ;; personal snippets
+        "~/.emacs.d/yasnippets"               ;; foo-mode and bar-mode snippet collection
+        ))
+
 (yas-global-mode 1)
 
 (message "Loading yasnippet settings... Done.")
