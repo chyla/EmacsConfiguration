63f9e23e39e74dea374a3b75dd32264621993994
Adam Chyła <adam@chyla.org>
2019-04-24 22:36:30 +0200

    disable gcc and clang checker for flycheck

--- site-start.d/50-flycheck.el
+++ site-start.d/50-flycheck.el
@@ -5,4 +5,7 @@
 ; when installed from Debian/Ubuntu packages
 (setq flycheck-rst-executable "rst2pseudoxml")
 
+; disable checkers
+(setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-gcc))
+
 (message "Loading flycheck settings... Done.")
