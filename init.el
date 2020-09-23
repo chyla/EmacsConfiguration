b85601795abe5e8236844b80f532ab534366297c
Adam Chyła <adam@chyla.org>
2020-09-23 15:08:21 +0200

    package-initialize up to Emacs 27

--- init.el
+++ init.el
@@ -5,7 +5,9 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+;; (package-initialize)
+(if (version< emacs-version "27.0")
+  (package-initialize))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
